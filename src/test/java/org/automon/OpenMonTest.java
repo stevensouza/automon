d72352749f95a207ae818d5d2c7ba6546c50212e
Steve Souza <steve@stevesouza.com>
2015-02-28 10:17:46 +0100

    added test - for OpenMon interface

---
+++ src/test/java/org/automon/OpenMonTest.java
@@ -0,0 +1,45 @@
+package org.automon;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OpenMonTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testStart() throws Exception {
+
+    }
+
+    @Test
+    public void testStop() throws Exception {
+
+    }
+
+    @Test
+    public void testException() throws Exception {
+
+    }
+
+    @Test
+    public void testEnable() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEnabled() throws Exception {
+
+    }
+}
