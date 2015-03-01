85f2098c77ceec4a9fbbc421df8d92e097016ee6
Steve Souza <steve@stevesouza.com>
2015-03-01 11:25:21 +0100

    refactor - simplified pointcut and made it more standard by using if(false) to disable

--- src/test/java/org/automon/aspects/MyDisabledTestAspect.java
+++ src/test/java/org/automon/aspects/MyDisabledTestAspect.java
@@ -5,29 +5,24 @@ import org.aspectj.lang.annotation.Pointcut;
 
 // Note the @Override annotation was not used below as it will not compile with ajc.
 class MyDisabledTestAspect extends AutomonAspect {
-    @Pointcut("none()")
+    @Pointcut("if(false)")
     public void sys_monitor() {
 
     }
 
-    @Pointcut("none()")
+    @Pointcut("if(false)")
     public void user_monitor() {
 
     }
 
-    @Pointcut("none()")
+    @Pointcut("if(false)")
     public void sys_exceptions() {
 
     }
 
     // @Pointcut("!within(java.lang.Object+)")
-    @Pointcut("none()")
+    @Pointcut("if(false)")
     public void user_exceptions() {
-    }
-
-    @Pointcut("if()")
-    public static boolean none() {
-        return false;
     }
 
 }
