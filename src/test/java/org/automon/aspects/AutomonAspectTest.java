82300de9c02ab4817b971634f78abe257c4ba511
Steve Souza <steve@stevesouza.com>
2015-03-01 09:39:34 +0100

    test - started aspect testing

---
+++ src/test/java/org/automon/aspects/AutomonAspectTest.java
@@ -0,0 +1,40 @@
+package org.automon.aspects;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AutomonAspectTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEnabled() throws Exception {
+
+    }
+
+    @Test
+    public void testEnable() throws Exception {
+
+    }
+
+    @Test
+    public void testMonitorPerformance() throws Exception {
+
+    }
+
+    @Test
+    public void testMonitorExceptions() throws Exception {
+
+    }
+}
