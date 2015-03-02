3b2c6a3a02a4ee2a667e9d76dad4805d72cb8459
Steve Souza <steve@stevesouza.com>
2015-03-02 11:49:13 +0100

    test - improved exception test and added test to make sure return value was returned properly

--- src/test/java/org/automon/aspects/AspectJBaseTest.java
+++ src/test/java/org/automon/aspects/AspectJBaseTest.java
@@ -15,7 +15,8 @@ import static org.mockito.Mockito.*;
 
 public class AspectJBaseTest {
 
-    public static final RuntimeException  TEST_RUNTIME_EXCEPTION = new RuntimeException("testing throwing exceptions");
+    private static final RuntimeException  TEST_RUNTIME_EXCEPTION = new RuntimeException("testing throwing exceptions");
+    private static final String RETURN_VALUE = "any string";
     private OpenMon openMon = mock(OpenMon.class);
 
     @Before
@@ -42,6 +43,15 @@ public class AspectJBaseTest {
     }
 
     @Test
+    public void testReturnValue() throws Exception {
+        MyTestClass obj = new MyTestClass();
+        String testString = obj.getString();
+        assertThat(testString).isEqualTo(RETURN_VALUE);
+        verify(openMon).start(any(JoinPoint.class));
+        verify(openMon).stop(any());
+    }
+
+    @Test
     public void testSys_exceptions() throws Exception {
         MyTestClass obj = new MyTestClass();
         try {
@@ -50,6 +60,8 @@ public class AspectJBaseTest {
             assertThat(t).isEqualTo(TEST_RUNTIME_EXCEPTION);
         }
 
+        verify(openMon).stop(any(), eq(TEST_RUNTIME_EXCEPTION));
+        verify(openMon, never()).stop(any());
         verify(openMon).exception(anyString());
     }
 
@@ -78,6 +90,10 @@ public class AspectJBaseTest {
 
         public void world() {
 
+        }
+
+        public String getString() {
+            return RETURN_VALUE;
         }
 
         private void iAmHiding() {
