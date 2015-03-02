8db4379c3c7d1131c8c257d23574dcec37eca7a1
Steve Souza <steve@stevesouza.com>
2015-03-02 10:00:50 +0100

    test - added test to see that exception handling logic is being handled in aspect

--- src/test/java/org/automon/aspects/AspectJBaseTest.java
+++ src/test/java/org/automon/aspects/AspectJBaseTest.java
@@ -3,6 +3,7 @@ package org.automon.aspects;
 import org.aspectj.lang.Aspects;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.automon.monitors.OpenMon;
 import org.automon.monitors.SysOut;
 import org.junit.After;
 import org.junit.Before;
@@ -10,16 +11,17 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class AspectJBaseTest {
+
+    public static final RuntimeException  TEST_RUNTIME_EXCEPTION = new RuntimeException("testing throwing exceptions");
+    private OpenMon openMon = mock(OpenMon.class);
 
     @Before
     public void setUp() throws Exception {
         MyAspectJTestAspect aspect = Aspects.aspectOf(MyAspectJTestAspect.class);
-        aspect.setOpenMon(new SysOut());
+        aspect.setOpenMon(openMon);
     }
 
     @After
@@ -33,17 +35,25 @@ public class AspectJBaseTest {
         obj.hello();
         obj.world();
         obj.iAmHiding();
+
+        // start/stop pair should be called once per public method due to public method pointcut.
+        verify(openMon, times(2)).start(anyString());
+        verify(openMon, times(2)).stop(any());
     }
 
     @Test
     public void testSys_exceptions() throws Exception {
+        MyTestClass obj = new MyTestClass();
+        try {
+            obj.throwException();
+        } catch (Throwable t) {
+            assertThat(t).isEqualTo(TEST_RUNTIME_EXCEPTION);
+        }
 
+        verify(openMon).exception(anyString());
     }
 
-    @Test
-    public void testIfEnabled() throws Exception {
 
-    }
 
     // Note the @Override annotation was not used below as it will not compile with ajc.
     @Aspect
@@ -72,6 +82,10 @@ public class AspectJBaseTest {
 
         private void iAmHiding() {
 
+        }
+
+        public void throwException() {
+            throw TEST_RUNTIME_EXCEPTION;
         }
     }
 }
