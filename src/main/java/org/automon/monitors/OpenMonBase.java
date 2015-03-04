24df275625c7d585de42d581d7f872e8671e260f
Steve Souza <steve@stevesouza.com>
2015-03-04 07:10:23 +0100

    refactor - changed back to using containsKey which is more expressive

--- src/main/java/org/automon/monitors/OpenMonBase.java
+++ src/main/java/org/automon/monitors/OpenMonBase.java
@@ -56,14 +56,43 @@ public abstract class OpenMonBase<T> implements OpenMon<T> {
         put(throwable);
     }
 
+    /**
+     * put in this class?
+     *
+     * should i change the map order to last is first inserted.  keeping in miind that anything over 2 minutes can go.
+     * The advantage is that i could abort the loop as soon as i hit the first one that didn't match?
+     *
+     *
+     * When iterating over the map the most recently accessed entry is returned first and the least recently accessed element is returned last.
+     *
+   private void trackException(Throwable rootCause, Method method, String sqlMessage) {
+     String detailStackTrace = Misc.getExceptionTrace(rootCause);
+     MonitorFactory.add(new MonKeyImp(MonitorFactory.EXCEPTIONS_LABEL, detailStackTrace, "Exception"), 1); // counts total exceptions from jamon
+     MonitorFactory.add(new MonKeyImp("MonProxy-Exception: InvocationTargetException", detailStackTrace, "Exception"), 1); //counts total exceptions for MonProxy
+     MonitorFactory.add(new MonKeyImp("MonProxy-Exception: Root cause exception="+rootCause.getClass().getName()+sqlMessage,
+     detailStackTrace, "Exception"), 1); // Message for the exception
+     MonitorFactory.add(new MonKeyImp(labelerInt.getExceptionLabel(method), detailStackTrace,"Exception"), 1); // Exception and method that threw it.
+     }
 
+     // Add special info if it is a SQLException
+     if (rootCause instanceof SQLException) {
+       SQLException sqlException = (SQLException) rootCause;
+       sqlMessage = ",ErrorCode=" + sqlException.getErrorCode()+ ",SQLState=" + sqlException.getSQLState();
+     }
+
+     // Add jamon entries for Exceptions
+     trackException(rootCause, method, sqlMessage);
+
+     */
+
+    //????
     public void exceptionImp(JoinPoint jp, Throwable throwable) {
 
     }
 
     protected void put(Throwable throwable) {
         // note 'get' is used instead of 'containsKey' as we want to update the LRU information for each access.
-        if (exceptionsMap.get(throwable)==null) {
+        if (!exceptionsMap.containsKey(throwable)) {
             exceptionsMap.put(throwable, new TimeExpirable());
         }
     }
