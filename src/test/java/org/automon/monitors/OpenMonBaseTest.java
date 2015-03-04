d66ae20bf6fb2d292265dfd3273d0b6d1f0d9087
Steve Souza <steve@stevesouza.com>
2015-03-04 14:01:14 +0100

    feature - forgot to commit new test files

--- src/test/java/org/automon/monitors/OpenMonBaseTest.java
+++ src/test/java/org/automon/monitors/OpenMonBaseTest.java
@@ -1,18 +1,50 @@
 package org.automon.monitors;
 
+import org.aspectj.lang.JoinPoint;
+import org.automon.utils.Expirable;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class OpenMonBaseTest {
+     OpenMonBase openMon = new OpenMonBase() {
+        @Override
+        public Object start(JoinPoint jp) {
+            return null;
+        }
+
+        @Override
+        public void stop(Object context) {
+
+        }
+    };
 
     @Test
     public void testException() throws Exception {
+        JoinPoint jp = mock(JoinPoint.class);
+        RuntimeException runtimeException = new RuntimeException("my exception");
+        openMon.exception(jp, runtimeException);
+        assertThat(openMon.getExceptionsMap()).containsOnlyKeys(runtimeException);
+        Map<Throwable, Expirable> exceptionsMap = openMon.getExceptionsMap();
+        Expirable expirable = exceptionsMap.get(runtimeException);
 
+        openMon.exception(jp, runtimeException);
+        openMon.exception(jp, runtimeException);
+
+        assertThat(openMon.getExceptionsMap()).containsOnlyKeys(runtimeException);
+        assertThat(openMon.getExceptionsMap().get(runtimeException)).
+                describedAs("The value object should only have been created once").
+                isEqualTo(expirable);
     }
 
     @Test
     public void testGetLabels() throws Exception {
-
+        RuntimeException runtimeException = new RuntimeException("my exception");
+        List<String> labels = openMon.getLabels(runtimeException);
+        assertThat(labels).containsOnly("java.lang.RuntimeException", OpenMon.EXCEPTION_LABEL);
     }
 }
--- src/test/java/org/automon/utils/UtilsTest.java
+++ src/test/java/org/automon/utils/UtilsTest.java
@@ -2,8 +2,11 @@ package org.automon.utils;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilsTest {
 
@@ -16,4 +19,17 @@ public class UtilsTest {
     public void tearDown() throws Exception {
 
     }
+
+    @Test
+    public void testBasicThrowableLabel() throws Exception {
+        RuntimeException runtimeException = new RuntimeException("My RTE exception");
+        assertThat(Utils.getLabel(runtimeException)).isEqualTo("java.lang.RuntimeException");
+    }
+
+    @Test
+    public void testSqlThrowableLabel() throws Exception {
+        SQLException sqlException = new SQLException("My SQL exception", "Login failure", 400);
+        assertThat(Utils.getLabel(sqlException)).isEqualTo("java.sql.SQLException,ErrorCode=400,SQLState=Login failure");
+    }
+
 }
