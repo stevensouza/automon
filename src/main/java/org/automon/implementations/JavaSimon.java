3b18ca90c75f08cec459841c4911bc3bb69b654c
Steve Souza <steve@stevesouza.com>
2015-03-11 10:43:53 +0100

    bug - fixed problem with changing to JavaSimon at runtime.  Changed it to throw the NoClassDefFoundError exception earlier - at object creation time as opposed to downstream in the code.   This made it possible to recover from the error.

--- src/main/java/org/automon/implementations/JavaSimon.java
+++ src/main/java/org/automon/implementations/JavaSimon.java
@@ -1,6 +1,7 @@
 package org.automon.implementations;
 
 import org.aspectj.lang.JoinPoint;
+import org.javasimon.Simon;
 import org.javasimon.SimonManager;
 import org.javasimon.Split;
 
@@ -10,6 +11,26 @@ import java.util.List;
  * Created by stevesouza on 2/26/15.
  */
 public class JavaSimon extends OpenMonBase<Split> {
+    /**
+     * <p>
+     * HACK ALERT: Fixes problem when user tries to create a JavaSimon when simon is not in the classpath.
+     *
+     * For some reason when JavaSimon jars aren't in the classpath the NoClassDefFoundError
+     * exception is not thrown at Object creation time but when 'trackException' is called.
+     * If this would happen instead when JavaSimon is created the exception is detected by {@link org.automon.implementations.OpenMonFactory}
+     * and a {@link org.automon.implementations.NullImp} implementation is returned. The advantage of this is that the
+     * program continues in a healthy manner.  This works fine with the other {@link org.automon.implementations.OpenMon} implementations.
+     * By putting a reference to simon as an instance variable the exception happens at JavaSimon creation time and the desired effect
+     * occurs.
+     * </p>
+     *
+     * <p>The exception this hack prevents is shown below:
+     *  Exception in thread "main" java.lang.NoClassDefFoundError: org/javasimon/SimonManager
+     *  at org.automon.implementations.JavaSimon.trackException(JavaSimon.java:28)
+     *  at org.automon.implementations.OpenMonBase.exception(OpenMonBase.java:43)
+     * </p>
+     */
+    private Simon hackToCauseNoClassDefFoundErrorOnCreation = SimonManager.getRootSimon();
 
     @Override
     public Split start(JoinPoint.StaticPart  jp) {
