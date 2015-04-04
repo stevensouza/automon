6d67a2170d066c31eba1bd0c6ca07757959c4d3b
Steve Souza <steve@stevesouza.com>
2015-04-04 18:24:30 -0400

    example: representative jetty startup script

---
+++ examples/jettystart_ltw.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#/Applications/myapps/jetty/jetty-distribution-9.2.6.v20141205/bin/jetty.sh start
+#!/bin/sh
+export JETTY_HOME=/Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609
+export JAVA_OPTIONS=' -server -Xms256m -Xmx512m -Dorg.aspectj.weaver.loadtime.configuration=file:/Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/resources/automon-aop.xml -javaagent:/Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/resources/aspectjweaver.jar '
+$JETTY_HOME/bin/jetty.sh start
