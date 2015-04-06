fbdc92b2a8dc907ff0b7b13b7b2dd4d93446ddea
Steve Souza <steve@stevesouza.com>
2015-04-06 12:49:26 -0400

    version change - changed *.sh files to reflect new version

--- examples/hello-world-unwoven-ltw.sh
+++ examples/hello-world-unwoven-ltw.sh
@@ -2,6 +2,6 @@
 
 # The following executes the HelloWorld using load time weaving (ltw).  The aspectj javaagent is required.
 # ltw is the most flexible way of using aspectj and is controled by passing an aspectj xml config line into the program.
-java   -Dorg.automon=sysout -Dorg.aspectj.weaver.loadtime.configuration=file:config/hello-world-unwoven-aop.xml -javaagent:libs/aspectjweaver.jar -classpath ../automon/target/automon-1.0.jar:../helloworld_unwoven/target/helloworld_unwoven-1.0.jar com.stevesouza.helloworld.HelloWorld
+java   -Dorg.automon=sysout -Dorg.aspectj.weaver.loadtime.configuration=file:config/hello-world-unwoven-aop.xml -javaagent:libs/aspectjweaver.jar -classpath ../automon/target/automon-1.0.1-SNAPSHOT.jar:../helloworld_unwoven/target/helloworld_unwoven-1.0.1-SNAPSHOT.jar com.stevesouza.helloworld.HelloWorld
 # to run the program in a loop for 100 times (allows time to look at automon jmx in jconsole)
-#java   -Dorg.automon=sysout -Dorg.aspectj.weaver.loadtime.configuration=file:config/hello-world-unwoven-aop.xml -javaagent:libs/aspectjweaver.jar -classpath ../automon/target/automon-1.0.jar:../helloworld_unwoven/target/helloworld_unwoven-1.0.jar com.stevesouza.helloworld.HelloWorld 100
+#java   -Dorg.automon=sysout -Dorg.aspectj.weaver.loadtime.configuration=file:config/hello-world-unwoven-aop.xml -javaagent:libs/aspectjweaver.jar -classpath ../automon/target/automon-1.0.1-SNAPSHOT.jar:../helloworld_unwoven/target/helloworld_unwoven-1.0.1-SNAPSHOT.jar com.stevesouza.helloworld.HelloWorld 100
