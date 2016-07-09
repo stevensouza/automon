d95428c202e4e9b8a70de1ccd2fb259060e48c1e
Steve Souza <steve@stevesouza.com>
2016-07-09 11:38:29 +0200

    update scripts to use the latest version of the automon jar (1.0.1)

--- examples/jdk-ltw.sh
+++ examples/jdk-ltw.sh
@@ -4,4 +4,4 @@
 # ltw is the most flexible way of using aspectj and is controled by passing an aspectj xml config line into the program.
 #
 # The example monitors jdk IO, and net classes.
-java  -Dorg.automon=statsd -Dorg.aspectj.weaver.loadtime.configuration=file:config/unwoven-jdk-aop.xml -javaagent:libs/aspectjweaver.jar -classpath ../automon/target/automon-1.0.1-SNAPSHOT.jar:../unwoven_jdk/target/unwoven_jdk-1.0.1-SNAPSHOT.jar:libs/hsqldb-2.3.2.jar:libs/java-statsd-client-3.1.0.jar com.stevesouza.jdk.JdkHelloWorld README.md
+java  -Dorg.automon=statsd -Dorg.aspectj.weaver.loadtime.configuration=file:config/unwoven-jdk-aop.xml -javaagent:libs/aspectjweaver.jar -classpath ../automon/target/automon-1.0.1.jar:../unwoven_jdk/target/unwoven_jdk-1.0.1.jar:libs/hsqldb-2.3.2.jar:libs/java-statsd-client-3.1.0.jar com.stevesouza.jdk.JdkHelloWorld README.md
