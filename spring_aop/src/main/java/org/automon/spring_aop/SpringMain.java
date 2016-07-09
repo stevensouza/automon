2801f7ade5bba41ab1e2065c21fa9bd0162b58c9
Steve Souza <steve@stevesouza.com>
2016-07-09 11:09:47 +0200

    Started work to get standard spring aop to work

---
+++ spring_aop/src/main/java/org/automon/spring_aop/SpringMain.java
@@ -0,0 +1,25 @@
+package org.automon.spring_aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by stevesouza on 5/24/14.
+ */
+public class SpringMain {
+
+    public static void main(String[] args) throws Exception {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // monitored not with annotation, but in applicationContext.xml
+        MonitorMe monitorMe = context.getBean("monitorMe", MonitorMe.class);
+
+        for (int i=0;i<10;i++) {
+            monitorMe.myMethod();
+            monitorMe.myName("steve", "souza");
+        }
+        
+        System.out.println(System.getProperties());
+
+
+    }
+}
