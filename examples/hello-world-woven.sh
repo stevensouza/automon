69e625434dcea7aea5ca9dca4e97ffdbc65eaa63
Steve Souza <steve@stevesouza.com>
2019-04-25 21:58:01 -0400

    made changes to prep for next version: 1.0.4

--- examples/hello-world-woven.sh
+++ examples/hello-world-woven.sh
@@ -2,7 +2,7 @@
 
 # Compile-time weaving - The following executes the HelloWorld woven application.  This means aspectj aspects were woven in at compile time and
 # are part of the classes. aspectjrt.jar needs to be in the classpath.
-java   -Dorg.automon=sysout -classpath ../automon/target/automon-1.0.3.jar:../helloworld_woven/target/helloworld_woven-1.0.3.jar:libs/aspectjrt.jar com.stevesouza.helloworld.HelloWorld
+java   -Dorg.automon=sysout -classpath ../automon/target/automon-1.0.4-SNAPSHOT.jar:../helloworld_woven/target/helloworld_woven-1.0.4-SNAPSHOT.jar:libs/aspectjrt.jar com.stevesouza.helloworld.HelloWorld
 
 # to run the program in a loop for 100 times (allows time to look at automon jmx in jconsole)
-# java   -Dorg.automon=sysout -classpath ../automon/target/automon-1.0.3.jar:../helloworld_woven/target/helloworld_woven-1.0.3.jar:libs/aspectjrt.jar com.stevesouza.helloworld.HelloWorld 100
+# java   -Dorg.automon=sysout -classpath ../automon/target/automon-1.0.4-SNAPSHOT.jar:../helloworld_woven/target/helloworld_woven-1.0.4-SNAPSHOT.jar:libs/aspectjrt.jar com.stevesouza.helloworld.HelloWorld 100
