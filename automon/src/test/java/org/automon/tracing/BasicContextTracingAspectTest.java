fc03ed05133abd1b6c7c0473752a8a09b135e8b5
Steve Souza <steve@stevesouza.com>
2024-08-21 16:24:48 -0400

    Pulled up common behavior into base test class.

--- automon/src/test/java/org/automon/tracing/BasicContextTracingAspectTest.java
+++ automon/src/test/java/org/automon/tracing/BasicContextTracingAspectTest.java
@@ -1,31 +1,15 @@
 package org.automon.tracing;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configuration;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-class BasicContextTracingAspectTest {
-
-    @BeforeEach
-    void setUp() {
-
-    }
-
-    @AfterEach
-    void tearDown() {
-        getListAppender().clear();
-    }
+class BasicContextTracingAspectTest extends TestTracingAspectBase {
 
     @Test
     public void testLogInfo() {
@@ -68,37 +52,6 @@ class BasicContextTracingAspectTest {
         };
 
         assertLogEvents(logEvents, expectedMessages);
-    }
-
-    private static void assertLogEvents(List<LogEvent> logEvents, String[] expectedMessages) {
-        for (int i = 0; i < logEvents.size(); i++) {
-            // executionTime=105 can vary so normalizing it for the assertions to executionTime=#
-            String actualMessage = logEvents.get(i).getMessage().getFormattedMessage();
-            actualMessage = actualMessage.replaceAll("executionTimeMs=\\d+", "executionTimeMs=#");
-
-            assertThat(actualMessage).
-                    describedAs("Each log event should start with this text (excludes ending newlines in check)").
-                    startsWith(expectedMessages[i]);
-        }
-    }
-
-    /**
-     * Retrieves the `ListAppender` with the name "ListAppender" from the Log4j 2 configuration.
-     *
-     * @return The `ListAppender` instance, or `null` if not found.
-     */
-    private static ListAppender getListAppender() {
-        // Get the LoggerContext (assuming Log4j 2 as the underlying logging implementation)
-        LoggerContext context = (LoggerContext) LogManager.getContext(false);
-
-        // Get the Configuration from the LoggerContext
-        Configuration configuration = context.getConfiguration();
-
-        // Get the appender by name from the Configuration
-        Appender appender = configuration.getAppender("ListAppender");
-
-        // Cast the Appender to ListAppender (assuming it's the correct type)
-        return (ListAppender) appender;
     }
 
     @Aspect
---
+++ automon/src/test/java/org/automon/tracing/TestTracingAspectBase.java
@@ -0,0 +1,56 @@
+package org.automon.tracing;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class TestTracingAspectBase {
+    protected static void assertLogEvents(List<LogEvent> logEvents, String[] expectedMessages) {
+        for (int i = 0; i < logEvents.size(); i++) {
+            // executionTime=105 can vary so normalizing it for the assertions to executionTime=#
+            String actualMessage = logEvents.get(i).getMessage().getFormattedMessage();
+            actualMessage = actualMessage.replaceAll("executionTimeMs=\\d+", "executionTimeMs=#");
+
+            assertThat(actualMessage).
+                    describedAs("Each log event should start with this text (excludes ending newlines in check)").
+                    startsWith(expectedMessages[i]);
+        }
+    }
+
+    /**
+     * Retrieves the `ListAppender` with the name "ListAppender" from the Log4j 2 configuration.
+     *
+     * @return The `ListAppender` instance, or `null` if not found.
+     */
+    protected static ListAppender getListAppender() {
+        // Get the LoggerContext (assuming Log4j 2 as the underlying logging implementation)
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+
+        // Get the Configuration from the LoggerContext
+        Configuration configuration = context.getConfiguration();
+
+        // Get the appender by name from the Configuration
+        Appender appender = configuration.getAppender("ListAppender");
+
+        // Cast the Appender to ListAppender (assuming it's the correct type)
+        return (ListAppender) appender;
+    }
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        TestTracingAspectBase.getListAppender().clear();
+    }
+}
