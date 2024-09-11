af6e7b5fa0f1753eaf1a6ff4cfdab0fb186df8bf
Steve Souza <steve@stevesouza.com>
2024-09-11 11:53:42 -0400

    prepping for spring aop to work...

---
+++ spring_woven/src/main/java/com/stevesouza/spring/FullContextTrace.java
@@ -0,0 +1,20 @@
+package com.mypackage.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+//import org.automon.tracing.spring.FullContextTracingAspect;
+import org.automon.tracing.spring.FullContextTracingAspect;
+
+@Aspect
+public class FullContextTrace extends FullContextTracingAspect {
+    // note to have method parameters written with their argument names you have to compile in debug mode
+    // (-g or in maven <compilerArgument>-parameters</compilerArgument>
+    //  Example with debug enabled: parameters={fname=steve, lname=souza}
+    // Without debug enabled you will still see the values but with generic parameter names such as
+    //  parameters={param0=steve, param1=souza}
+    @Pointcut("execution(public * com.stevesouza.helloworld.HelloWorld.*(..))")
+    public void select() {
+
+    }
+
+}
