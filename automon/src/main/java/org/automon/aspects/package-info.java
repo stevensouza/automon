c39c2449818fc7223b91c3e1a3d0bae1042f4d36
Steve Souza <steve@stevesouza.com>
2024-09-23 14:27:27 -0400

    adding more javadocs for classes (generated from gemini ai) one package at a time...

--- automon/src/main/java/org/automon/aspects/package-info.java
+++ automon/src/main/java/org/automon/aspects/package-info.java
@@ -2,10 +2,10 @@
  * This package serves as the container for all AspectJ aspects within Automon, encompassing both monitoring and tracing functionalities.<p>
  * It includes sub-packages for:
  * <ul>
- * <li><code>monitoring</code>: Contains aspects focused on collecting performance metrics and monitoring method invocations and exceptions.</li>
- * <li><code>tracing</code>: Houses aspects for tracing method executions and exceptions, providing contextual information for logging and debugging.</li>
- * <li><code>tracing.aspectj</code>: Offers specialized tracing aspects for non-Spring applications, extending tracing capabilities beyond Spring AOP's limitations.</li>
- * <li><code>tracing.spring</code>: Contains Spring-specific tracing aspects, seamlessly integrating tracing with Spring AOP. Note these can also be used
+ * <li><code>org.automon.aspects.monitoring</code>: Contains aspects focused on collecting performance metrics and monitoring method invocations and exceptions.</li>
+ * <li><code>org.automon.aspects.tracing</code>: Houses aspects for tracing method executions and exceptions, providing contextual information for logging and debugging.</li>
+ * <li><code>org.automon.aspects.tracing.aspectj</code>: Offers specialized tracing aspects for non-Spring applications, extending tracing capabilities beyond Spring AOP's limitations.</li>
+ * <li><code>org.automon.aspects.tracing.spring</code>: Contains Spring-specific tracing aspects, seamlessly integrating tracing with Spring AOP. Note these can also be used
  * in non-Spring applications too.</li>
  * </ul>
  */
